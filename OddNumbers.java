1677acfceac22b2b5acefcd1b153ba45e7127ffa
virshikarajeshkumar <virshikarajeshkumar@gmail.com>
2025-01-25 14:46:32 +0530

    Add files via upload

---
+++ OddNumbers.java
@@ -0,0 +1,14 @@
+public class OddNumbers 
+{
+    public static void main(String[] args) 
+{
+        System.out.println("Odd numbers from 1 to 100:");
+        for (int i = 1; i <= 100; i++) 
+   {
+            if (i % 2 != 0)
+     {
+              System.out.print(i + " ");
+      }
+    }
+ }
+}
